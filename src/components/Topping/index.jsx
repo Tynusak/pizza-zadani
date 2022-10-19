a65eb051c4f295ca9c9ee2be3966565d79e59d13
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-10-19 22:22:06 +0200

    Druhý statelifting

--- src/components/Topping/index.jsx
+++ src/components/Topping/index.jsx
@@ -2,14 +2,10 @@ import React, { useState } from 'react';
 import Check from '../Check';
 import './style.css';
 
-const Topping = ({ topping }) => {
-  const [checked, setChecked] = useState(false);
-  const handleCheck = () => {
-    setChecked(!checked);
-  };
+const Topping = ({ topping, onCheckChange }) => {
   return (
     <div className="topping">
-      <Check checked={checked} onChange={handleCheck} />
+      <Check checked={topping.selected} onChange={onCheckChange} />
       <span className="topping__content">
         {topping.name}: {topping.price} €
       </span>
--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -3,14 +3,26 @@ import Topping from '../Topping';
 import './style.css';
 
 const ToppingsSelect = ({ toppings }) => {
+  const [checked, setChecked] = useState(toppings);
+
+  const handleCheck = (index) => {
+    const newToppings = [...toppings];
+    newToppings[index].selected = !checked[index].selected;
+    setChecked(newToppings);
+  };
+
   return (
     <>
       <p>Choose as many toppings as you want</p>
       <p>Selected toppings: 0, total price: 0 Euro</p>
 
       <div className="toppings">
-        {toppings.map((topping) => (
-          <Topping topping={topping} key={topping.name} />
+        {toppings.map((topping, index) => (
+          <Topping
+            topping={topping}
+            key={topping.name}
+            onCheckChange={() => handleCheck(index)}
+          />
         ))}
       </div>
     </>
